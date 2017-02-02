ca6cd110f96ec902ec4bb6b50c76c6003066d67e
KAYATHRI <1313050@saec.ac.in>
2017-02-02 12:32:13 +0530

    Add files via upload

---
+++ ideone_8VpPeb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your co
+	int num;
+scanf("%d",&num);
+	if(num>0)
+	printf("positive num");
+	else if(num<0)
+   printf("negative num");
+    else
+	printf("zero num");
+	return 0;
+}
